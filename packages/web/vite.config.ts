ae58773f0f0b1be759172555e3a8cdbcb0c1fae4
thisissteven <ssteven075@gmail.com>
2026-08-02 21:53:11 +0800

    chore: edit configs

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,8 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      routesDirectory: './src/routes',
+      generatedRouteTree: './src/routeTree.gen.ts',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tailwindcss(),
+  ],
 });
